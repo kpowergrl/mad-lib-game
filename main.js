2b470ee57f5286a49be7b3f1e3d9910a6331326f
Kelly Power <kpowergrl@gmail.com>
2019-01-20 20:36:45 -0800

    added button to mad lib game

---
+++ main.js
@@ -0,0 +1,34 @@
+  //eslint-disable-next-line
+  function madlibstory() {
+    
+    // get the inputs by id
+    var noun1Input = document.getElementById('noun-1');
+    var noun2Input = document.getElementById('noun-2');
+    var verbInput = document.getElementById('verb');
+    
+    //get the values out of inputs
+    var noun1 = noun1Input.value;
+    var noun2 = noun2Input.value;
+    var verb = verbInput.value;
+    
+    console.log(noun1, noun2, verb);
+
+
+    // //make greeting
+    // var greeting = 'Hello ' + firstName + ' ' + lastName + '!';
+    // console.log(greeting);
+
+
+    // //display greeting
+    // var greeter = document.getElementById('greeter');
+    // greeter.textContent = greeting;
+        
+
+    // remove prompt
+
+
+}
+
+// TODO: validate name inputs
+// TODO: clear inputs after greet?
+    
